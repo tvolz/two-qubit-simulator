405ebe91e0845b055c479c640a1cd9b49ac7ec5b
bwouterlood <brendan.wouterlood@students.mq.edu.au>
2019-01-30 16:03:40 +1100

    further hadamard

--- two_qubit_simulator/quantum_gates/hadamard.py
+++ two_qubit_simulator/quantum_gates/hadamard.py
@@ -10,6 +10,8 @@ class Hadamard(QuantumGate):
     def __init__(self):
 
         ''' DO THINGS HERE'''
+        symbol = "H"
+        unitary_operator = (1/np.sqrt(2))* np.array([[1, 1], [1, -1]])
 
 
         super(Hadamard, self).__init__(unitary_operator, symbol)
