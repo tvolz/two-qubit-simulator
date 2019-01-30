1deca63b4ca8d9a6cd70b287d61090b587310c12
bwouterlood <brendan.wouterlood@students.mq.edu.au>
2019-01-30 15:51:32 +1100

    made Haddamard gate

--- two_qubit_simulator/quantum_gates/hadamard.py
+++ two_qubit_simulator/quantum_gates/hadamard.py
@@ -6,4 +6,12 @@ from .quantum_gate import QuantumGate
 
 class Hadamard(QuantumGate):
     """ Implements the Hadamard gate """
+
+    def __init__(self):
+
+        ''' DO THINGS HERE'''
+
+
+        super(Hadamard, self).__init__(unitary_operator, symbol)
+
     pass
