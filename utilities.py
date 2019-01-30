49e390adc16296107b5464233ef4900583233d97
BSnowdon <benjamin.snowdon@hdr.mq.edu.au>
2019-01-30 15:11:12 +1100

    Added utilities module with conjugate transpose

---
+++ utilities.py
@@ -0,0 +1,23 @@
+"""
+# utilities.py
+Contains utility functions for the two_qubit_simulator module
+"""
+
+import numpy as np
+
+def conjugate_transpose(array):
+    """ Calculates the conjugate transpose of an array
+
+        Parameters
+        -------
+        array : numpy darray
+            The array to be transposed
+        
+        Returns
+        -------
+        numpy darray
+            The conjugate transpose of the input array
+    """
+    return np.conjugate(
+        np.transpose(array)
+    )
